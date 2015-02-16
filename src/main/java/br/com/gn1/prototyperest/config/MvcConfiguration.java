71d5cf72256ac5f0fa4d5b10d0cf55c282fc5801
Daniel Marcoto <daniel@gn1.com.br>
2015-02-16 19:57:43 -0200

    Excluding unnecessary import

--- src/main/java/br/com/gn1/prototyperest/config/MvcConfiguration.java
+++ src/main/java/br/com/gn1/prototyperest/config/MvcConfiguration.java
@@ -9,7 +9,6 @@ import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
